397ff84991a6cefce273c9dbf29b32e161c8700a
Han Chen <chenhan28@gmail.com>
2021-10-06 01:05:48 +0800

    improvements

--- R/fgit.R
+++ R/fgit.R
@@ -3,32 +3,49 @@ fgit_clone <- function(
   overwite = FALSE,
   verbose = TRUE
 ) {
+  # Set the domain of fastgit
   fast_git_domain <-
     "https://hub.fastgit.org/"
 
+  # Jointing the Repo name and FastGit
+  # as the url of Repo
   fast_repo <-
     paste0(fast_git_domain, repo)
 
+  # Create tmp dir
   tmp_dir <-
     file.path(tempdir(), repo)
 
-  if(dir.exists(tmp_dir) && isFALSE(overwite)){
-    err_msg <-
-      paste(tmp_dir,
-            "exists! Please resolve it or set `overwrite = TRUE` to overwrite automatically")
-    stop(err_msg)
-  } else {
-    if(dir.exists(tmp_dir) && isTRUE(overwite)) {
+  # In case target dir exists
+  # if overwrite is FALSE
+  # report an Error
+
+  if (dir.exists(tmp_dir)) {
+    if (isFALSE(overwite)) {
+      err_msg <-
+        paste(tmp_dir, "exists! Please resolve it.
+              Or set `overwrite = TRUE` to overwrite automatically")
+      stop(err_msg)
+    } else {
+      # If overwrite is TRUE
+      # remove target dir
       rm_cmd <-
         paste("rm -rf", tmp_dir)
       system(rm_cmd)
-      if(isTRUE(verbose)){
+      # Report a message
+      if (isTRUE(verbose)) {
         message(tmp_dir, "has been empty.")
       }
     }
-    dir.create(tmp_dir, recursive = TRUE)
-    clone_cmd <-
-      paste0("git -C ", tmp_dir, " clone ", fast_repo)
-    system(clone_cmd)
   }
+  # Create target dir
+  dir.create(tmp_dir, recursive = TRUE)
+
+  # Assemble the Git clone command
+  # use -C argument to set the
+  # target dir
+  clone_cmd <-
+    paste0("git -C ", tmp_dir, " clone ", fast_repo)
+  system(clone_cmd)
+  message(paste0("Repo has been cloned to", tmp_dir))
 }
